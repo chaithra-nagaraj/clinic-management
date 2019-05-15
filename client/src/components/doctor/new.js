feeb180fe486fdf8a40a43277c4c047911a922a9
chaithra <chaithranagaraj.cn@gmail.com>
2019-05-15 14:48:26 +0530

    new.js file added

---
+++ client/src/components/doctor/new.js
@@ -0,0 +1,15 @@
+import react from 'react'
+
+class New extends React.Component {
+    render(){
+        render(
+            <div>
+                
+                    <h1>Welcome to new page</h1>
+                
+            </div>
+        )
+    }
+}
+
+export default New
